f790875f556b09b4eadb71b41cca057e7df7967a
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-06-13 21:00:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-def caseTransfer(text,text2):
-    count = 0
-    for i in range(len(text)):
-        if text[i]!=text2[i]:
-            count+=1
-    print(count)
-    
-    
-
-caseTransfer('river','rover')
+num = [1,1,2]
+print(list(set(num)))
