3b00eb5599ff6e6365dd9e5efcd69198ae4dc1a9
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-07-06 18:38:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print([1]+[])
+def replaceElements(arr): 
+        
+        if len(arr) == 1:
+            return -1
+       
+        arr[-1] = -1
+
+        for i in range(len(arr)-2, -1,-1):
+
+            temp = max(arr[i],arr[i-1])
+            arr[i] = temp
+            
+            
+        print(arr)
+
+replaceElements([17,18,5,4,6,1])
