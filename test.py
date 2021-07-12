f49e9c6c337bcbcb103f1ad9ce1167c1d978c4f7
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-07-12 09:37:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 matrix  = [[1,2,3],[4,5,6],[7,8,9]]
 
 print([list(x) for x in (zip(*matrix[::-1]))])
@@ -7,6 +7,4 @@ print([list(x) for x in (zip(*matrix[::-1]))])
 
 
 #matrix[:] = []
-=======
 
->>>>>>> d65c77e13d8319addff80eb18637772c6c967700
